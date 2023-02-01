ef6e399ada30ae90d308ce60af80677d6ccf3142
Kaburu <mbayakelvin@gmail.com>
2023-02-01 20:36:29 +0300

    adjust the weight to find a sweet spot

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def processImage(url,uploadID,uploadName,zipObj):
     # Use a cascading classifier to detect objects within the image
     face_cascade = cv2.CascadeClassifier(os.getenv("face_cascade"))
 
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.01, minNeighbors=7, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.05, minNeighbors=8, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
     print("Found {} faces!".format(len(faces)))
     if len(faces) > 0:
         largest_face = max(faces, key=lambda x: x[2] * x[3])
